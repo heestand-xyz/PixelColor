674f6cfd6619e73888a61cc95261d046d682c3a4
Anton Heestand <anton.heestand@gmail.com>
2021-06-21 12:46:36 +0200

    Dark Mode

---
+++ Sources/PixelColor/PixelColor+DarkMode.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anton Heestand on 2021-06-21.
+//
+
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
+
+extension PixelColor {
+    
+    public enum Appearance {
+        case light
+        case dark
+    }
+    
+    public static var appearance: Appearance {
+        #if os(macOS)
+        return NSApp.effectiveAppearance.name == .darkAqua ? .dark : .light
+        #else
+        return UIView().traitCollection.userInterfaceStyle == .dark ? .dark : .light
+        #endif
+    }
+}
