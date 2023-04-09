d5549f89855714934f44d4f80c3f854cfa91a594
Anton Heestand <anton.heestand@gmail.com>
2023-04-09 14:07:32 +0900

    Colors

--- Sources/PixelColor/Codable/CodableColor.swift
+++ Sources/PixelColor/Codable/CodableColor.swift
@@ -1,8 +1,15 @@
 import CoreGraphics
+import SwiftUI
 
 public enum CodableColor: Codable, Equatable, Hashable {
     case `static`(PixelColor)
     case dynamic(DynamicColor, opacity: CGFloat)
+}
+
+extension CodableColor {
+    public init(color: Color) {
+        self = CodableColor.decode(color: color) ?? .static(.gray)
+    }
 }
 
 extension CodableColor {
