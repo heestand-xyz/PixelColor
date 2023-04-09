f83098e0ae0b863afc8f4389f25fb9fe05aa19e6
Anton Heestand <anton.heestand@gmail.com>
2023-04-09 11:30:19 +0900

    Color

--- Sources/PixelColor/Codable/CodableColor.swift
+++ Sources/PixelColor/Codable/CodableColor.swift
@@ -1,6 +1,6 @@
 import CoreGraphics
 
-public enum CodableColor: Codable {
+public enum CodableColor: Codable, Equatable, Hashable {
     case `static`(PixelColor)
     case dynamic(DynamicColor, opacity: CGFloat)
 }
